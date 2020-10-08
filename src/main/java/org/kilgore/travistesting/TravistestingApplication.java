d8ffb64ba2f0086fe2b7bb68e92068e62eac76cb
Jeff Kilgore <kilgorjn@gmail.com>
2020-10-07 20:42:34 -0500

    first commit

---
+++ src/main/java/org/kilgore/travistesting/TravistestingApplication.java
@@ -0,0 +1,13 @@
+package org.kilgore.travistesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravistestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravistestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kilgore/travistesting/TravistestingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kilgore.travistesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravistestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
